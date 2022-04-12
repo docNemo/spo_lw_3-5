6b4f9ebeb267d4319a9c3011b4234886d32f1b9e
Nemo <vayag300300@yandex.ru>
2022-04-12 10:22:48 +0300

    update args func

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import lexer
 from parser import Parser
 
 # lexer = lexer.Lexer("$func asdf(id1, id2, id3, id4) = id1 * id2 - id3 ^ id4");
+lexer = lexer.Lexer("asdf(id1, 4, id3, id4)");
 parser = parser.Parser(lexer);
 print(parser.parse());
--- parser.py
+++ parser.py
@@ -5,51 +5,54 @@ from collections import deque
 
 
 def stateToString(state):
-  return [ ".","%eof","E","mul","E","E","E","E","E","E","E","E","E","plus","minus","minus","del","assign","id","let","pow","id","lparen","rparen","Args","assign","func","id","lparen","rparen","Args","id","comma","Args1","Args1","number","lparen","rparen" ][state]
+  return [ ".","%eof","E","mul","E","E","E","E","E","E","E","E","E","plus","minus","minus","del","assign","id","let","pow","id","lparen","rparen","Args","assign","func","id","lparen","rparen","Args","id","number","comma","comma","Args1","Args1","Args1","number","lparen","rparen" ][state]
 
 def expectedSym(state):
-  return [ "E","%eof","%eof/mul/plus/minus/del/pow","E","mul/%eof/del/minus/mul/plus/pow/rparen/plus/minus/del/pow","mul/plus/%eof/del/minus/mul/plus/pow/rparen/minus/del/pow","mul/plus/minus/%eof/del/minus/mul/plus/pow/rparen/del/pow","mul/plus/minus/%eof/del/minus/mul/plus/pow/rparen/del/pow","mul/plus/minus/del/%eof/del/minus/mul/plus/pow/rparen/pow","mul/plus/minus/del/%eof/del/minus/mul/plus/pow/rparen/pow","mul/plus/minus/del/pow/%eof/del/minus/mul/plus/pow/rparen","mul/plus/minus/del/pow/%eof/del/minus/mul/plus/pow/rparen","mul/plus/minus/del/pow/rparen","E","E","E","E","E","assign","id","E","lparen/%eof/del/minus/mul/plus/pow/rparen","Args","%eof/del/minus/mul/plus/pow/rparen","rparen","E","id","lparen","Args","assign","rparen","rparen/comma","Args1","rparen","rparen","%eof/del/minus/mul/plus/pow/rparen","E","%eof/del/minus/mul/plus/pow/rparen" ][state]
+  return [ "E","%eof","%eof/mul/plus/minus/del/pow","E","mul/%eof/del/minus/mul/plus/pow/rparen/plus/minus/del/pow","mul/plus/%eof/del/minus/mul/plus/pow/rparen/minus/del/pow","mul/plus/minus/%eof/del/minus/mul/plus/pow/rparen/del/pow","mul/plus/minus/%eof/del/minus/mul/plus/pow/rparen/del/pow","mul/plus/minus/del/%eof/del/minus/mul/plus/pow/rparen/pow","mul/plus/minus/del/%eof/del/minus/mul/plus/pow/rparen/pow","mul/plus/minus/del/pow/%eof/del/minus/mul/plus/pow/rparen","mul/plus/minus/del/pow/%eof/del/minus/mul/plus/pow/rparen","mul/plus/minus/del/pow/rparen","E","E","E","E","E","assign","id","E","lparen/%eof/del/minus/mul/plus/pow/rparen","Args","%eof/del/minus/mul/plus/pow/rparen","rparen","E","id","lparen","Args","assign","rparen","rparen/comma","rparen/comma","Args1","Args1","rparen","rparen","rparen","%eof/del/minus/mul/plus/pow/rparen","E","%eof/del/minus/mul/plus/pow/rparen" ][state]
 
 class Parser:
     Action = [
-        [38,36,38,38,38,38,15,38,38,38,21,35,19,26],
-        [39,38,38,38,38,38,38,38,38,38,38,38,38,38],
-        [1,38,38,3,13,38,14,16,38,20,38,38,38,38],
-        [38,36,38,38,38,38,15,38,38,38,21,35,19,26],
-        [40,38,40,40,40,38,40,40,38,20,38,38,38,38],
-        [41,38,41,3,41,38,41,16,38,20,38,38,38,38],
-        [42,38,42,3,42,38,42,16,38,20,38,38,38,38],
-        [43,38,43,43,43,38,43,43,38,43,38,38,38,38],
-        [44,38,44,44,44,38,44,44,38,20,38,38,38,38],
-        [45,38,45,3,13,38,14,16,38,20,38,38,38,38],
-        [46,38,46,46,46,38,46,46,38,20,38,38,38,38],
-        [47,38,47,3,13,38,14,16,38,20,38,38,38,38],
-        [38,38,37,3,13,38,14,16,38,20,38,38,38,38],
-        [38,36,38,38,38,38,15,38,38,38,21,35,19,26],
-        [38,36,38,38,38,38,15,38,38,38,21,35,19,26],
-        [38,36,38,38,38,38,15,38,38,38,21,35,19,26],
-        [38,36,38,38,38,38,15,38,38,38,21,35,19,26],
-        [38,36,38,38,38,38,15,38,38,38,21,35,19,26],
-        [38,38,38,38,38,38,38,38,17,38,38,38,38,38],
-        [38,38,38,38,38,38,38,38,38,38,18,38,38,38],
-        [38,36,38,38,38,38,15,38,38,38,21,35,19,26],
-        [48,22,48,48,48,38,48,48,38,48,38,38,38,38],
-        [38,38,49,38,38,38,38,38,38,38,31,38,38,38],
-        [50,38,50,50,50,38,50,50,38,50,38,38,38,38],
-        [38,38,23,38,38,38,38,38,38,38,38,38,38,38],
-        [38,36,38,38,38,38,15,38,38,38,21,35,19,26],
-        [38,38,38,38,38,38,38,38,38,38,27,38,38,38],
-        [38,28,38,38,38,38,38,38,38,38,38,38,38,38],
-        [38,38,49,38,38,38,38,38,38,38,31,38,38,38],
-        [38,38,38,38,38,38,38,38,25,38,38,38,38,38],
-        [38,38,29,38,38,38,38,38,38,38,38,38,38,38],
-        [38,38,51,38,38,32,38,38,38,38,38,38,38,38],
-        [38,38,38,38,38,38,38,38,38,38,31,38,38,38],
-        [38,38,52,38,38,38,38,38,38,38,38,38,38,38],
-        [38,38,53,38,38,38,38,38,38,38,38,38,38,38],
-        [54,38,54,54,54,38,54,54,38,54,38,38,38,38],
-        [38,36,38,38,38,38,15,38,38,38,21,35,19,26],
-        [55,38,55,55,55,38,55,55,38,55,38,38,38,38]
+        [41,39,41,41,41,41,15,41,41,41,21,38,19,26],
+        [42,41,41,41,41,41,41,41,41,41,41,41,41,41],
+        [1,41,41,3,13,41,14,16,41,20,41,41,41,41],
+        [41,39,41,41,41,41,15,41,41,41,21,38,19,26],
+        [43,41,43,43,43,41,43,43,41,20,41,41,41,41],
+        [44,41,44,3,44,41,44,16,41,20,41,41,41,41],
+        [45,41,45,3,45,41,45,16,41,20,41,41,41,41],
+        [46,41,46,46,46,41,46,46,41,46,41,41,41,41],
+        [47,41,47,47,47,41,47,47,41,20,41,41,41,41],
+        [48,41,48,3,13,41,14,16,41,20,41,41,41,41],
+        [49,41,49,49,49,41,49,49,41,20,41,41,41,41],
+        [50,41,50,3,13,41,14,16,41,20,41,41,41,41],
+        [41,41,40,3,13,41,14,16,41,20,41,41,41,41],
+        [41,39,41,41,41,41,15,41,41,41,21,38,19,26],
+        [41,39,41,41,41,41,15,41,41,41,21,38,19,26],
+        [41,39,41,41,41,41,15,41,41,41,21,38,19,26],
+        [41,39,41,41,41,41,15,41,41,41,21,38,19,26],
+        [41,39,41,41,41,41,15,41,41,41,21,38,19,26],
+        [41,41,41,41,41,41,41,41,17,41,41,41,41,41],
+        [41,41,41,41,41,41,41,41,41,41,18,41,41,41],
+        [41,39,41,41,41,41,15,41,41,41,21,38,19,26],
+        [51,22,51,51,51,41,51,51,41,51,41,41,41,41],
+        [41,41,52,41,41,41,41,41,41,41,31,32,41,41],
+        [53,41,53,53,53,41,53,53,41,53,41,41,41,41],
+        [41,41,23,41,41,41,41,41,41,41,41,41,41,41],
+        [41,39,41,41,41,41,15,41,41,41,21,38,19,26],
+        [41,41,41,41,41,41,41,41,41,41,27,41,41,41],
+        [41,28,41,41,41,41,41,41,41,41,41,41,41,41],
+        [41,41,52,41,41,41,41,41,41,41,31,32,41,41],
+        [41,41,41,41,41,41,41,41,25,41,41,41,41,41],
+        [41,41,29,41,41,41,41,41,41,41,41,41,41,41],
+        [41,41,54,41,41,33,41,41,41,41,41,41,41,41],
+        [41,41,55,41,41,34,41,41,41,41,41,41,41,41],
+        [41,41,41,41,41,41,41,41,41,41,31,32,41,41],
+        [41,41,41,41,41,41,41,41,41,41,31,32,41,41],
+        [41,41,56,41,41,41,41,41,41,41,41,41,41,41],
+        [41,41,57,41,41,41,41,41,41,41,41,41,41,41],
+        [41,41,58,41,41,41,41,41,41,41,41,41,41,41],
+        [59,41,59,59,59,41,59,59,41,59,41,41,41,41],
+        [41,39,41,41,41,41,15,41,41,41,21,38,19,26],
+        [60,41,60,60,60,41,60,60,41,60,41,41,41,41]
         ]
     GOTO = [
         [2,0,0],
@@ -74,17 +77,20 @@ class Parser:
         [0,0,0],
         [10,0,0],
         [0,0,0],
-        [0,24,34],
+        [0,24,37],
         [0,0,0],
         [0,0,0],
         [11,0,0],
         [0,0,0],
         [0,0,0],
-        [0,30,34],
+        [0,30,37],
         [0,0,0],
         [0,0,0],
         [0,0,0],
-        [0,0,33],
+        [0,0,0],
+        [0,0,35],
+        [0,0,36],
+        [0,0,0],
         [0,0,0],
         [0,0,0],
         [0,0,0],
@@ -104,10 +110,10 @@ class Parser:
         a = self.lex.getNextToken()
         while True:
             action = self.Action[self.top()][int(a[0])]
-            if action == 39:
+            if action == 42:
                 self.stack.pop()
                 return self.stack.pop()[1]
-            elif action == 49:
+            elif action == 52:
                 if self.debug: print("Reduce using Args -> ")
                 
                 gt = self.GOTO[self.top()][1] # Args
@@ -116,7 +122,7 @@ class Parser:
                     print(f'{self.top()} is now on top of the stack;')
                     print(f'{gt} will be placed on the stack')
                 self.stack.append((gt,(None)))
-            elif action == 53:
+            elif action == 58:
                 if self.debug: print("Reduce using Args -> Args1")
                 _1 = self.stack.pop()[1]
                 gt = self.GOTO[self.top()][1] # Args
@@ -125,7 +131,7 @@ class Parser:
                     print(f'{self.top()} is now on top of the stack;')
                     print(f'{gt} will be placed on the stack')
                 self.stack.append((gt,(_1)))
-            elif action == 51:
+            elif action == 54:
                 if self.debug: print("Reduce using Args1 -> id")
                 _1 = self.stack.pop()[1][1]
                 gt = self.GOTO[self.top()][2] # Args1
@@ -134,7 +140,7 @@ class Parser:
                     print(f'{self.top()} is now on top of the stack;')
                     print(f'{gt} will be placed on the stack')
                 self.stack.append((gt,([_1])))
-            elif action == 52:
+            elif action == 56:
                 if self.debug: print("Reduce using Args1 -> id comma Args1")
                 _3 = self.stack.pop()[1]
                 _2 = self.stack.pop()[1][1]
@@ -145,7 +151,27 @@ class Parser:
                     print(f'{self.top()} is now on top of the stack;')
                     print(f'{gt} will be placed on the stack')
                 self.stack.append((gt,([_1] + _3)))
-            elif action == 47:
+            elif action == 55:
+                if self.debug: print("Reduce using Args1 -> number")
+                _1 = self.stack.pop()[1][1]
+                gt = self.GOTO[self.top()][2] # Args1
+                if gt==0: raise Exception("No goto")
+                if self.debug:
+                    print(f'{self.top()} is now on top of the stack;')
+                    print(f'{gt} will be placed on the stack')
+                self.stack.append((gt,([_1])))
+            elif action == 57:
+                if self.debug: print("Reduce using Args1 -> number comma Args1")
+                _3 = self.stack.pop()[1]
+                _2 = self.stack.pop()[1][1]
+                _1 = self.stack.pop()[1][1]
+                gt = self.GOTO[self.top()][2] # Args1
+                if gt==0: raise Exception("No goto")
+                if self.debug:
+                    print(f'{self.top()} is now on top of the stack;')
+                    print(f'{gt} will be placed on the stack')
+                self.stack.append((gt,([_1] + _3)))
+            elif action == 50:
                 if self.debug: print("Reduce using E -> func id lparen Args rparen assign E")
                 _7 = self.stack.pop()[1]
                 _6 = self.stack.pop()[1][1]
@@ -160,7 +186,7 @@ class Parser:
                     print(f'{self.top()} is now on top of the stack;')
                     print(f'{gt} will be placed on the stack')
                 self.stack.append((gt,((_2, _4, _7))))
-            elif action == 48:
+            elif action == 51:
                 if self.debug: print("Reduce using E -> id")
                 _1 = self.stack.pop()[1][1]
                 gt = self.GOTO[self.top()][0] # E
@@ -169,7 +195,7 @@ class Parser:
                     print(f'{self.top()} is now on top of the stack;')
                     print(f'{gt} will be placed on the stack')
                 self.stack.append((gt,(_1)))
-            elif action == 50:
+            elif action == 53:
                 if self.debug: print("Reduce using E -> id lparen Args rparen")
                 _4 = self.stack.pop()[1][1]
                 _3 = self.stack.pop()[1]
@@ -181,7 +207,7 @@ class Parser:
                     print(f'{self.top()} is now on top of the stack;')
                     print(f'{gt} will be placed on the stack')
                 self.stack.append((gt,((_1, _3))))
-            elif action == 45:
+            elif action == 48:
                 if self.debug: print("Reduce using E -> let id assign E")
                 _4 = self.stack.pop()[1]
                 _3 = self.stack.pop()[1][1]
@@ -193,7 +219,7 @@ class Parser:
                     print(f'{self.top()} is now on top of the stack;')
                     print(f'{gt} will be placed on the stack')
                 self.stack.append((gt,(('=', _2, _4))))
-            elif action == 55:
+            elif action == 60:
                 if self.debug: print("Reduce using E -> lparen E rparen")
                 _3 = self.stack.pop()[1][1]
                 _2 = self.stack.pop()[1]
@@ -204,7 +230,7 @@ class Parser:
                     print(f'{self.top()} is now on top of the stack;')
                     print(f'{gt} will be placed on the stack')
                 self.stack.append((gt,(_2)))
-            elif action == 43:
+            elif action == 46:
                 if self.debug: print("Reduce using E -> minus E")
                 _2 = self.stack.pop()[1]
                 _1 = self.stack.pop()[1][1]
@@ -214,7 +240,7 @@ class Parser:
                     print(f'{self.top()} is now on top of the stack;')
                     print(f'{gt} will be placed on the stack')
                 self.stack.append((gt,(('-', _2))))
-            elif action == 54:
+            elif action == 59:
                 if self.debug: print("Reduce using E -> number")
                 _1 = self.stack.pop()[1][1]
                 gt = self.GOTO[self.top()][0] # E
@@ -223,7 +249,7 @@ class Parser:
                     print(f'{self.top()} is now on top of the stack;')
                     print(f'{gt} will be placed on the stack')
                 self.stack.append((gt,(_1)))
-            elif action == 44:
+            elif action == 47:
                 if self.debug: print("Reduce using E -> E del E")
                 _3 = self.stack.pop()[1]
                 _2 = self.stack.pop()[1][1]
@@ -234,7 +260,7 @@ class Parser:
                     print(f'{self.top()} is now on top of the stack;')
                     print(f'{gt} will be placed on the stack')
                 self.stack.append((gt,(('/', _1, _3))))
-            elif action == 42:
+            elif action == 45:
                 if self.debug: print("Reduce using E -> E minus E")
                 _3 = self.stack.pop()[1]
                 _2 = self.stack.pop()[1][1]
@@ -245,7 +271,7 @@ class Parser:
                     print(f'{self.top()} is now on top of the stack;')
                     print(f'{gt} will be placed on the stack')
                 self.stack.append((gt,(('-', _1, _3))))
-            elif action == 40:
+            elif action == 43:
                 if self.debug: print("Reduce using E -> E mul E")
                 _3 = self.stack.pop()[1]
                 _2 = self.stack.pop()[1][1]
@@ -256,7 +282,7 @@ class Parser:
                     print(f'{self.top()} is now on top of the stack;')
                     print(f'{gt} will be placed on the stack')
                 self.stack.append((gt,(('*', _1, _3))))
-            elif action == 41:
+            elif action == 44:
                 if self.debug: print("Reduce using E -> E plus E")
                 _3 = self.stack.pop()[1]
                 _2 = self.stack.pop()[1][1]
@@ -267,7 +293,7 @@ class Parser:
                     print(f'{self.top()} is now on top of the stack;')
                     print(f'{gt} will be placed on the stack')
                 self.stack.append((gt,(('+', _1, _3))))
-            elif action == 46:
+            elif action == 49:
                 if self.debug: print("Reduce using E -> E pow E")
                 _3 = self.stack.pop()[1]
                 _2 = self.stack.pop()[1][1]
@@ -278,7 +304,7 @@ class Parser:
                     print(f'{self.top()} is now on top of the stack;')
                     print(f'{gt} will be placed on the stack')
                 self.stack.append((gt,(('^', _1, _3))))
-            elif action == 38:
+            elif action == 41:
                 lastSt = self.top()
                 parsed=stateToString(lastSt)
                 while len(self.stack) > 0:
