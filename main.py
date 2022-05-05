c35bc31d06170bc4304191e1aa90e0e031aa0d74
Nemo <vayag300300@yandex.ru>
2022-05-05 16:18:53 +0300

    all ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from parser import *
 from lexer import *
+from get3Code import *
 
 input = ''' let a1 = 3 * 4.0;
             func f(a1:int) = a1 * a1;
@@ -10,7 +11,6 @@ input = ''' let a1 = 3 * 4.0;
             a2 + 4 ^ f1(a2);
             f(2);
             func f2(f:(float) -> float) = f(5);
-            f2(f1);
             func f3() = 5;
             f3()
             '''
@@ -19,12 +19,20 @@ parser = Parser()
 trees = parser.parse(lex(input))
 
 variables = {}
+compileVar = {}
 
 print(trees)
 for tree in trees:
-    if isinstance(tree, Function) or isinstance(tree, Assign):
+    if isinstance(tree, Function):
         variables[tree.Value] = tree.type(variables)
+        compileVar[tree.Value] = getFCode(tree, variables)
+    elif isinstance(tree, Assign):
+        variables[tree.Value] = tree.type(variables)
+        compileVar[tree.Value] = getProgram(tree.Expr, variables)
     else:
         print("\n###################################\n")
         print(tree)
         print(tree.type(variables))
+        code = getProgram(tree, variables)
+        print(code)
+        print(interp(code, compileVar))
